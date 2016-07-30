9c308b3ca9df2e636d0a30e08e4caf96db284c42
myronliu <myron@liuyongpengdeMacBook-Pro.local>
2016-07-30 23:49:06 +0800

    调整样式

--- app/components/item.js
+++ app/components/item.js
@@ -8,13 +8,22 @@ export default class item extends React.Component {
   }
   gotoPage(){
     window.location = this.props.data.url;
-  }
+  }//<img src={} />
   render(){
     // console.log(this.props.data)
+    var bgI={
+      backgroundImage: "url('http://7xskmo.com1.z0.glb.clouddn.com/" + this.props.data.thumb_media_id + ".jpg')",
+      // width: '50px',
+      // height: '50px',
+      // overflow: 'hidden',
+      // backgroundRepeat: 'no-repeat',
+      // backgroundPosition: 'center center',
+      // backgroundSize: 'cover',
+    }
     return (
       <TapAble onTap={this.gotoPage.bind(this)}>
-        <div className={this.props.data.thumb_media_id ? "sucai-img" : "hide"}>
-          <img src={'http://7xskmo.com1.z0.glb.clouddn.com/' + this.props.data.thumb_media_id + '.jpg'} />
+        <div className={this.props.data.thumb_media_id ? "sucai-img" : "hide"} style={bgI}>
+          
         </div>
         <div className="sucai-content">
           <div className="sucai-title">
